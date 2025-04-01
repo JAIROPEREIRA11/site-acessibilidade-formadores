9517f006efc66bf8bd6943254bdc5e1ea5f00327
JAIROPEREIRA11 <105813968+JAIROPEREIRA11@users.noreply.github.com>
2025-04-01 16:40:58 -0300

    atualizar botao

--- script.js
+++ script.js
@@ -11,4 +11,11 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize= `${tamanhoAtualFonte}rem`;
 })
+const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidae');
+
+botaoDeAcessibilidade.addEventListener('click',function(){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+})
 })
